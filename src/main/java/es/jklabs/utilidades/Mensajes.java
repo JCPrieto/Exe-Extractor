78ee467c5e0c7f691b8603d47bd48335ff4c944e
Juan Carlos Prieto <JuanC.Prieto.Silos@gmail.com>
2026-06-01 21:23:11 +0200

    Refactor: simplify `getError` method by removing redundant `resource` parameter and adding a private constructor

--- src/main/java/es/jklabs/utilidades/Mensajes.java
+++ src/main/java/es/jklabs/utilidades/Mensajes.java
@@ -5,16 +5,20 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Mensajes {
-    public static String getError(String key) {
-        return getResource("i18n/errores", key);
+
+    private Mensajes() {
     }
 
-    private static String getResource(String resource, String key) {
+    public static String getError(String key) {
+        return getResource(key);
+    }
+
+    private static String getResource(String key) {
         if (key == null || key.isBlank()) {
             return "";
         }
         try {
-            ResourceBundle bundle = ResourceBundle.getBundle(resource, Locale.getDefault());
+            ResourceBundle bundle = ResourceBundle.getBundle("i18n/errores", Locale.getDefault());
             return bundle.getString(key);
         } catch (MissingResourceException e) {
             return key;
